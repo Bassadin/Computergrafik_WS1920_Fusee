f6a548d03fd923b41a6adbe8e0cb893b8e30e352
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-29 19:37:00 +0100

    Initial commit

---
+++ Tut08_FirstSteps/Tut08_FirstSteps.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Fusee.Base.Common;
+using Fusee.Base.Core;
+using Fusee.Engine.Common;
+using Fusee.Engine.Core;
+using Fusee.Math.Core;
+using Fusee.Serialization;
+using Fusee.Xene;
+using static Fusee.Engine.Core.Input;
+using static Fusee.Engine.Core.Time;
+using Fusee.Engine.GUI;
+using System.Collections.Generic;
+using System.Linq;
+using Fusee.Tutorial.Core;
+using System.Diagnostics;
+
+namespace FuseeApp
+{
+
+    public class FirstSteps : RenderCanvas
+    {
+        private SceneContainer _scene;
+        private SceneRendererForward _sceneRenderer;
+
+        private float _camAngle = 0;
+
+        // Init is called on startup. 
+        public override void Init()
+        {
+            // Set the clear color for the backbuffer to light green (intensities in R, G, B, A).
+            RC.ClearColor = new float4(255, 255, 255, 1.0f);
+
+            // Create a scene with a cube
+            // The three components: one XForm, one Shader and the Mesh
+            var cubeTransform = new TransformComponent
+            {
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(0, 0, 0),
+                Rotation = new float3(0, 0, 0)
+            };
+            var cubeShader = new ShaderEffectComponent
+            {
+                Effect = SimpleMeshes.MakeShaderEffect(new float3(1, 0, 0), new float3(1, 1, 1), 4)
+            };
+            var cubeMesh = SimpleMeshes.CreateCuboid(new float3(10, 10, 10));
+
+            // Assemble the cube node containing the three components
+            var cubeNode = new SceneNodeContainer();
+            cubeNode.Components = new List<SceneComponentContainer>();
+            cubeNode.Components.Add(cubeTransform);
+            cubeNode.Components.Add(cubeShader);
+            cubeNode.Components.Add(cubeMesh);
+
+            // Create the scene containing the cube as the only object
+            _scene = new SceneContainer();
+            _scene.Children = new List<SceneNodeContainer>();
+            _scene.Children.Add(cubeNode);
+
+            // Create a scene renderer holding the scene above
+            _sceneRenderer = new SceneRendererForward(_scene);
+        }
+
+        // RenderAFrame is called once a frame
+        public override void RenderAFrame()
+        {
+            SetProjectionAndViewport();
+
+            // Clear the backbuffer
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            // foreach (var item in _scene.Children)
+            // {
+            //     Diagnostics.Debug(item);
+            //     item.GetComponent<TransformComponent>().Translation = new float3(0, 5 * M.Sin(3 * TimeSinceStart), 0);
+            // }
+
+            // Setup the camera 
+            _camAngle += 90.0f * M.Pi / 180.0f * DeltaTime;
+
+            RC.View = float4x4.CreateTranslation(0, 0, 50) * float4x4.CreateRotationY(_camAngle);
+            Debug.WriteLine(_camAngle);
+
+            // Render the scene on the current render context
+            _sceneRenderer.Render(RC);
+
+            // Swap buffers: Show the contents of the backbuffer (containing the currently rendered farame) on the front buffer.
+            Present();
+        }
+
+        public void SetProjectionAndViewport()
+        {
+            // Set the rendering area to the entire window size
+            RC.Viewport(0, 0, Width, Height);
+
+            // Create a new projection matrix generating undistorted images on the new aspect ratio.
+            var aspectRatio = Width / (float)Height;
+
+            // 0.25*PI Rad -> 45° Opening angle along the vertical direction. Horizontal opening angle is calculated based on the aspect ratio
+            // Front clipping happens at 1 (Objects nearer than 1 world unit get clipped)
+            // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
+            var projection = float4x4.CreatePerspectiveFieldOfView(M.PiOver4, aspectRatio, 1, 20000);
+            RC.Projection = projection;
+        }
+    }
+}
