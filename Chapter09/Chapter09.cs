2b41831c134e5386cd9df87506a96da3cf19dac0
Bastian Hodapp <bastianhodapp@gmail.com>
2019-12-03 15:23:30 +0100

    Added Mr. Grabby

--- Chapter09/Chapter09.cs
+++ Chapter09/Chapter09.cs
@@ -17,42 +17,70 @@ namespace FuseeApp
     [FuseeApplication(Name = "Tut09_HierarchyAndInput", Description = "Yet another FUSEE App.")]
     public class Tut09_HierarchyAndInput : RenderCanvas
     {
-        private SceneContainer _scene;
-        private SceneRendererForward _sceneRenderer;
+        private SceneContainer scene;
+        private SceneRendererForward sceneRenderer;
         private float _camAngle = 0;
-        private TransformComponent _baseTransform;
-        private TransformComponent _bodyTransform;
-        private TransformComponent _upperArmTransform;
-        private TransformComponent _lowerArmTransform;
+        private TransformComponent baseTransform;
+        private TransformComponent bodyTransform;
+
+        //Arms
+        private TransformComponent upperArmTransform;
+        private TransformComponent lowerArmTransform;
+
+        //Grabbers
+        private TransformComponent grabber1Transform;
+        private TransformComponent grabber2Transform;
+
+        private enum GrabberState : int { open = 0, closing = 1, closed = 2, opening = 3 };
+        private GrabberState currentGrabberState = GrabberState.open;
+
+        //Once the grabber color
+        private float3 grabberColor = new float3(0.2f, 0.2f, 0.2f);
+        private float3 grabberCuboidSize = new float3(3, 8, 3);
 
 
         SceneContainer CreateScene()
         {
             // Initialize transform components that need to be changed inside "RenderAFrame"
-            _baseTransform = new TransformComponent
+            baseTransform = new TransformComponent
             {
                 Rotation = new float3(0, 0, 0),
                 Scale = new float3(1, 1, 1),
-                Translation = new float3(0, 0, 0)
+                Translation = new float3(0, -6, 0)
             };
 
-            _bodyTransform = new TransformComponent
+            bodyTransform = new TransformComponent
             {
                 Rotation = new float3(0, 0, 0),
                 Scale = new float3(1, 1, 1),
                 Translation = new float3(0, 6, 0)
             };
-            _upperArmTransform = new TransformComponent
+            //Arms
+            upperArmTransform = new TransformComponent
             {
                 Rotation = new float3(0, 0, 0),
                 Scale = new float3(1, 1, 1),
-                Translation = new float3(2, 8, 0)
+                Translation = new float3(2, 4, 0)
             };
-            _lowerArmTransform = new TransformComponent
+            lowerArmTransform = new TransformComponent
             {
                 Rotation = new float3(0, 0, 0),
                 Scale = new float3(1, 1, 1),
-                Translation = new float3(-2, 8, 0)
+                Translation = new float3(-2, 4, 0)
+            };
+
+            //Grabbers
+            grabber1Transform = new TransformComponent
+            {
+                Rotation = new float3(-1, 0, 0),
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(0, 4, 0)
+            };
+            grabber2Transform = new TransformComponent
+            {
+                Rotation = new float3(1, 0, 0),
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(0, 4, 0)
             };
 
             // Setup the scene graph
@@ -66,7 +94,7 @@ namespace FuseeApp
                         Components = new List<SceneComponentContainer>
                         {
                             // TRANSFROM COMPONENT
-                            _baseTransform,
+                            baseTransform,
 
                             // SHADER EFFECT COMPONENT
                             new ShaderEffectComponent
@@ -85,7 +113,7 @@ namespace FuseeApp
                                 Components = new List<SceneComponentContainer>
                                 {
                                     // TRANSFROM COMPONENT
-                                    _bodyTransform,
+                                    bodyTransform,
 
                                     // SHADER EFFECT COMPONENT
                                     new ShaderEffectComponent
@@ -104,38 +132,118 @@ namespace FuseeApp
                                         Name = "UpperArm",
                                         Components = new List<SceneComponentContainer>
                                         {
-                                            // TRANSFROM COMPONENT
-                                            _upperArmTransform,
-
-                                            // SHADER EFFECT COMPONENT
-                                            new ShaderEffectComponent
-                                            {
-                                                Effect = SimpleMeshes.MakeShaderEffect(new float3(0, 1, 0), new float3(0.7f, 0.7f, 0.7f), 5)
-                                            },
-
-                                            // MESH COMPONENT
-                                            SimpleMeshes.CreateCuboid(new float3(2, 10, 2))
+                                            upperArmTransform
 
                                         },
                                         Children = new ChildList
                                         {
                                             new SceneNodeContainer
                                             {
-                                                Name = "LowerArm",
                                                 Components = new List<SceneComponentContainer>
                                                 {
-                                                    // TRANSFROM COMPONENT
-                                                    _lowerArmTransform,
-
-                                                    // SHADER EFFECT COMPONENT
+                                                    new TransformComponent
+                                                    {
+                                                        Rotation = new float3(0, 0, 0),
+                                                        Scale = new float3(1, 1, 1),
+                                                        Translation = new float3(0, 4, 0)
+                                                    },
                                                     new ShaderEffectComponent
                                                     {
-                                                        Effect = SimpleMeshes.MakeShaderEffect(new float3(0, 0, 1), new float3(0.7f, 0.7f, 0.7f), 5)
+                                                        Effect = SimpleMeshes.MakeShaderEffect(new float3(0, 1, 0), new float3(0.7f, 0.7f, 0.7f), 5)
                                                     },
-
-                                                    // MESH COMPONENT
                                                     SimpleMeshes.CreateCuboid(new float3(2, 10, 2))
+                                                },
+                                                Children = new ChildList {
+                                                    new SceneNodeContainer
+                                                    {
+                                                        Name = "LowerArm",
+                                                        Components = new List<SceneComponentContainer>
+                                                        {
+                                                            lowerArmTransform
 
+                                                        },
+                                                        Children = new ChildList
+                                                        {
+                                                            new SceneNodeContainer
+                                                            {
+                                                                Components = new List<SceneComponentContainer>
+                                                                {
+                                                                    new TransformComponent
+                                                                    {
+                                                                        Rotation = new float3(0, 0, 0),
+                                                                        Scale = new float3(1, 1, 1),
+                                                                        Translation = new float3(0, 4, 0)
+                                                                    },
+                                                                    new ShaderEffectComponent
+                                                                    {
+                                                                        Effect = SimpleMeshes.MakeShaderEffect(new float3(0, 0, 1), new float3(0.7f, 0.7f, 0.7f), 5)
+                                                                    },
+                                                                    SimpleMeshes.CreateCuboid(new float3(2, 10, 2))
+                                                                },
+                                                                Children = new ChildList
+                                                                {
+                                                                    new SceneNodeContainer
+                                                                        {
+                                                                            Name = "Grabber1",
+                                                                            Components = new List<SceneComponentContainer>
+                                                                            {
+                                                                                grabber1Transform
+
+                                                                            },
+                                                                            Children = new ChildList
+                                                                            {
+                                                                                new SceneNodeContainer
+                                                                                {
+                                                                                    Components = new List<SceneComponentContainer>
+                                                                                    {
+                                                                                        new TransformComponent
+                                                                                        {
+                                                                                            Rotation = new float3(0, 0, 0),
+                                                                                            Scale = new float3(1, 1, 1),
+                                                                                            Translation = new float3(0, 4, 0)
+                                                                                        },
+                                                                                        new ShaderEffectComponent
+                                                                                        {
+                                                                                            Effect = SimpleMeshes.MakeShaderEffect(grabberColor, new float3(0.7f, 0.7f, 0.7f), 5)
+                                                                                        },
+                                                                                        SimpleMeshes.CreateCuboid(grabberCuboidSize)
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        },
+                                                                    new SceneNodeContainer
+                                                                        {
+                                                                            Name = "Grabber2",
+                                                                            Components = new List<SceneComponentContainer>
+                                                                            {
+                                                                                grabber2Transform
+
+                                                                            },
+                                                                            Children = new ChildList
+                                                                            {
+                                                                                new SceneNodeContainer
+                                                                                {
+                                                                                    Components = new List<SceneComponentContainer>
+                                                                                    {
+                                                                                        new TransformComponent
+                                                                                        {
+                                                                                            Rotation = new float3(0, 0, 0),
+                                                                                            Scale = new float3(1, 1, 1),
+                                                                                            Translation = new float3(0, 4, 0)
+                                                                                        },
+                                                                                        new ShaderEffectComponent
+                                                                                        {
+                                                                                            Effect = SimpleMeshes.MakeShaderEffect(grabberColor, new float3(0.7f, 0.7f, 0.7f), 5)
+                                                                                        },
+                                                                                        SimpleMeshes.CreateCuboid(grabberCuboidSize)
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
                                                 }
                                             }
                                         }
@@ -155,10 +263,10 @@ namespace FuseeApp
             // Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
             RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
 
-            _scene = CreateScene();
+            scene = CreateScene();
 
             // Create a scene renderer holding the scene above
-            _sceneRenderer = new SceneRendererForward(_scene);
+            sceneRenderer = new SceneRendererForward(scene);
         }
 
         // RenderAFrame is called once a frame
@@ -169,11 +277,54 @@ namespace FuseeApp
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
+            //Rotate robot axes
+            upperArmTransform.Rotation.x += 1.5f * DeltaTime * Keyboard.UpDownAxis;
+            lowerArmTransform.Rotation.x += 1.5f * DeltaTime * Keyboard.WSAxis;
+
+            //Rotate camera with mouse
+            if (Mouse.LeftButton)
+            {
+                _camAngle += -.01f * DeltaTime * Mouse.Velocity.x;
+            }
+
+            //Diagnostics.Debug(currentGrabberState);
+
+            //Grabber movement
+            if (Keyboard.IsKeyDown(KeyCodes.Space))
+            {
+                currentGrabberState = currentGrabberState == GrabberState.open || currentGrabberState == GrabberState.opening ? GrabberState.closing : GrabberState.opening;
+                Diagnostics.Debug("Setting Grabber state to " + currentGrabberState);
+            }
+
+            if (currentGrabberState == GrabberState.closing)
+            {
+                grabber1Transform.Rotation.x += .5f * DeltaTime;
+                grabber2Transform.Rotation.x -= .5f * DeltaTime;
+                if (grabber1Transform.Rotation.x >= 0)
+                {
+                    Diagnostics.Debug("Setting Grabber state to closed");
+                    currentGrabberState = GrabberState.closed;
+                    grabber1Transform.Rotation.x = 0;
+                    grabber2Transform.Rotation.x = 0;
+                }
+            } else if (currentGrabberState == GrabberState.opening) {
+                grabber1Transform.Rotation.x -= .5f * DeltaTime;
+                grabber2Transform.Rotation.x += .5f * DeltaTime;
+                if (grabber1Transform.Rotation.x <= -1)
+                {
+                    Diagnostics.Debug("Setting Grabber state to open");
+                    currentGrabberState = GrabberState.open;
+                    grabber1Transform.Rotation.x = -1;
+                    grabber2Transform.Rotation.x = 1;
+                }
+            }
+
+
             // Setup the camera 
             RC.View = float4x4.CreateTranslation(0, -10, 50) * float4x4.CreateRotationY(_camAngle);
 
             // Render the scene on the current render context
-            _sceneRenderer.Render(RC);
+            sceneRenderer.Render(RC);
 
             // Swap buffers: Show the contents of the backbuffer (containing the currently rendered farame) on the front buffer.
             Present();
